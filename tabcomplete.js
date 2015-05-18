a86cb4e867f01273c1271cf8a05b315c9be37258
CBenni <bensnik@web.de>
2015-05-18 12:06:19 +0200

    Fixed a bug with contenteditable inputs and general optimizations

--- tabcomplete.js
+++ tabcomplete.js
@@ -4,57 +4,60 @@ $.widget("cbenni.tabcomplete", {
 	},
 	_create: function() 
 	{
-		this.textsplit = ["", "", ""];
-		this.tabtries = -1;
+		var textsplit = ["", "", ""];
+		var tabtries = -1;
 		var self = this;
 		this.element.bind("click focus", function() {
 			self.tabtries = -1;
 		});
-		
-		this.element.keydown(function(plugin){return function(e) {
+		var textfunction = null;
+		this.element.keydown(function(e) {
 			var code = e.keyCode || e.which;
 			if (code == 9) { // tab pressed
 				e.preventDefault();
 				// if this is the first time tab is pressed here, we split the text before and after the word
-				if (plugin.tabtries == -1) {
-					var caretpos = $(this).caret();
-					var text = $(this).val()||$(this).text();
+				if (tabtries == -1) {
+					$this = $(this);
+					var caretpos = $this.caret();
+					if($this.is("input, select, textarea"))
+						textfunction = function(v){return (arguments.length?$this.val(v):$this.val());};
+					else 
+						textfunction = function(v){return (arguments.length?$this.text(v):$this.text());};
+					var text = textfunction();
 					var start = (/\w+$/.exec(text.substr(0, caretpos)) || {index: caretpos}).index;
 					var end = caretpos + (/^\w+/.exec(text.substr(caretpos)) || [""])[0].length;
-					plugin.textsplit = [text.substring(0, start), text.substring(start, end), text.substring(end + 1)];
+					textsplit = [text.substring(0, start), text.substring(start, end), text.substring(end + 1)];
 				}
 				// calculate the collection of strings actually eligible for suggestion, either by filtering or by executing the function specified
-				var collection = plugin.options.collection || [];
+				var collection = self.options.collection || [];
 				if(typeof collection === "object")
 				{
 					collection = collection.filter(function(v){
-						return v.toLowerCase().indexOf(plugin.textsplit[1].toLowerCase())==0;
+						return v.toLowerCase().indexOf(textsplit[1].toLowerCase())==0;
 					});
 				}
 				else if (typeof collection == "function") 
-					collection = collection(plugin.textsplit[1]);
+					collection = collection(textsplit[1]);
 				// collection now (hopefully) is a list of values
 				if (collection.length > 0) {
 					// shift key iterates backwards
-					plugin.tabtries += e.shiftKey?-1:1;
-					if(plugin.tabtries >= collection.length) plugin.tabtries = 0;
-					if(plugin.tabtries < 0) plugin.tabtries = collection.length+plugin.tabtries;
-					$(this).val(plugin.textsplit[0] + collection[plugin.tabtries] + plugin.textsplit[2]);
-					$(this).text(plugin.textsplit[0] + collection[plugin.tabtries] + plugin.textsplit[2]);
-					$(this).caret(plugin.textsplit[0].length + collection[plugin.tabtries].length);
+					tabtries += e.shiftKey?-1:1;
+					if(tabtries >= collection.length) tabtries = 0;
+					if(tabtries < 0) tabtries = collection.length+tabtries;
+					textfunction(textsplit[0] + collection[tabtries] + textsplit[2]);
+					$(this).caret(textsplit[0].length + collection[tabtries].length);
 				}
 			}
 			// escape
-			else if(code == 27 && plugin.tabtries>=0)
+			else if(code == 27 && tabtries>=0)
 			{
-				$(this).val(plugin.textsplit[0] + plugin.textsplit[1] + plugin.textsplit[2]);
-				$(this).text(plugin.textsplit[0] + plugin.textsplit[1] + plugin.textsplit[2]);
+				textfunction(textsplit[0] + textsplit[1] + textsplit[2]);
 			}
 			// not shift
 			else if(code != 16)
 			{
-				plugin.tabtries = -1;
+				tabtries = -1;
 			}
-		}}(this));
+		});
 	}
 });
